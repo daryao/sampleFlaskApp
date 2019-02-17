e4730ed2eb1df3abe12462524e3ad1f013f7f1d6
daryao <hnyhazel@gmail.com>
2019-02-16 22:26:29 -0700

    New templates, css

--- hello-flask.py
+++ hello-flask.py
@@ -1,15 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from vsearch import search4letters
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-
-@app.route(‘/search4’)
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
-    return str(search4letters(‘life, the universe, and everything’, ‘eiru,!’))
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Results: '
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html', the_phrase = phrase, the_letters = letters, the_title = title, the_results = results,)
 
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+        the_title='Welcome to search4letters on the web!')
 
-app.run()
+app.run(debug=True)
